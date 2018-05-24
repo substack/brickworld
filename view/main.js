e7e8ac87ea0f305db7cf59441a2996eef69f8558
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2018-05-24 13:09:41 -1000

    blur, not defocus

--- view/main.js
+++ view/main.js
@@ -76,7 +76,7 @@ module.exports = function (state, emit) {
   function rotate () { emit('rotate-brick') }
   function toggleRemove () { emit('toggle-remove') }
   function selectBrick () {
-    this.defocus()
+    if (this.blur) this.blur()
     emit('select-brick', this.value.split(' ').map(Number))
   }
   function clear () { emit('clear') }
